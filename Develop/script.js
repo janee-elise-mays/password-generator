1da486890ff4b6e7cc4ea5b706b9db1cf7cb2157
Janeé Mays <janee.elise@gmail.com>
2021-02-09 15:23:55 -0500

    altered js function

--- Develop/script.js
+++ Develop/script.js
@@ -1,29 +1,33 @@
 // Assignment Code
 var generateBtn = document.querySelector("generate");
+var passwordFiled = document.getElementById("password");
+var passwordLength = document.getElementById("passwordLength");
+
 
 // Write password to the #password input
 function writePassword() {
+ 
   var password = '';
   var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' +  
                     'abcdefghijklmnopqrstuvwxyz0123456789@#$'; 
               
-            for (i = 1; i <= 8; i++) { 
+            for (i = 1; i <= passwordLength.value; i++) { 
                 var char = Math.floor(Math.random() 
                             * str.length + 1); 
                   
-                pass += str.charAt(char) 
+                password += str.charAt(char) 
             } 
               
-            return pass; 
+            passwordFiled.value = password; 
         } 
   // var passwordText = document.querySelector("#password");
 
   // passwordText.value = password;
 
-}
+// }
 
 // Add event listener to generate button
-generateBtn("generate").addEventListener("click", writePassword() {
-  alert (password)
-});
-</script>
+//generateBtn("generate").addEventListener("click", writePassword() {
+//  alert (password)
+//});
+//</script>
